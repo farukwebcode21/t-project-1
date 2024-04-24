5ba454c9b3d0ed02f39376c71c494b87d5a06acb
farukwebcode21 <farukahmadmk@yahoo.com>
2024-04-24 16:47:31 +0600

    complete font basic price

--- dist/script.js
+++ dist/script.js
@@ -31,3 +31,12 @@ const showReview = () => {
   userPics[i].classList.add("active-pic");
   userTexts[i].classList.add("active-text");
 };
+
+const toggleBtn = document.getElementById("toggleBtn");
+const card_1_front = document.getElementById("card_1_front");
+const card_1_back = document.getElementById("card_1_back");
+
+toggleBtn.addEventListener("change", () => {
+  card_1_front.classList.toggle("-rotate-y-180");
+  card_1_back.classList.toggle("rotate-y-180");
+});
